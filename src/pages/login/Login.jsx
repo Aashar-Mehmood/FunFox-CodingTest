0345e78b77a59343b7baf8229d0edae7c117873b
Aashar-Mehmood <aasharmehmood3@gmail.com>
2023-04-17 11:22:39 +0500

    created login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,73 @@
+import "./login.css";
+import React from "react";
+import { Row, Col, Typography, Button, Form, Input, Space } from "antd";
+import { MailTwoTone, LockTwoTone } from "@ant-design/icons";
+const { Title } = Typography;
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
 export default function Login() {
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <section id="login-container">
+      <Row className="form-container">
+        <Col xs={22} sm={16} md={12} lg={8}>
+          <Space direction="vertical" size="large" style={{ display: "flex" }}>
+            <Title level={2}>Login</Title>
+            <Form
+              name="login"
+              layout="vertical"
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Space
+                direction="vertical"
+                size="small"
+                style={{ display: "flex" }}
+              >
+                <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your email!",
+                    },
+                    {
+                      type: "email",
+                      message: "Enter a valid email!",
+                    },
+                  ]}
+                >
+                  <Input size="large" prefix={<MailTwoTone />} />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input.Password size="large" prefix={<LockTwoTone />} />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button type="primary" htmlType="submit" size="large">
+                    Login
+                  </Button>
+                </Form.Item>
+              </Space>
+            </Form>
+          </Space>
+        </Col>
+      </Row>
+    </section>
   );
 }
